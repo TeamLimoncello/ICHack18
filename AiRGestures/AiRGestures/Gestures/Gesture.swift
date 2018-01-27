d607a375c595b1226675a643e8fa6a8ab7815e79
Jay Lees <jay.lees@me.com>
2018-01-27 14:01:17 +0000

    Gesture recognition interface

---
+++ AiRGestures/AiRGestures/Gestures/Gesture.swift
@@ -0,0 +1,20 @@
+//
+//  Gesture.swift
+//  AiRGestures
+//
+//  Created by Jay Lees on 27/01/2018.
+//  Copyright © 2018 Lewis Bell. All rights reserved.
+//
+
+import Foundation
+
+
+public enum Gesture {
+    case thumb
+    case oneFinger
+    case twoFingers
+    case threeFingers
+    case fourFingers
+    case fiveFingers
+    case fist
+}
---
+++ AiRGestures/AiRGestures/Gestures/GestureRecogniser.swift
@@ -0,0 +1,29 @@
+//
+//  GestureRecogniser.swift
+//  AiRGestures
+//
+//  Created by Jay Lees on 27/01/2018.
+//  Copyright © 2018 Lewis Bell. All rights reserved.
+//
+
+import Foundation
+import CoreImage
+
+public class GestureRecogniser {
+    var model: GestureModel
+    
+    
+    init(){
+        self.model = GestureModel()
+    }
+    
+    public func detectGestures(in image: CVPixelBuffer) -> Gesture? {
+        do {
+            let prediction = try model.prediction(data: image)
+            print(prediction.classLabel)
+        } catch let error {
+            print(error)
+        }
+        return nil
+    }
+}
