bcedce6bbae04cf3e1542da9bc056e2004fa0dd3
Lewis Bell <lewisbell999@gmail.com>
2018-01-27 13:47:37 +0000

    Set camera up

--- AiRGestures/AiRGestures/ViewController.swift
+++ AiRGestures/AiRGestures/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var session: AVCaptureSession!
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //Camera Setup
+        session = AVCaptureSession()
+        session!.sessionPreset = AVCaptureSession.Preset.high
+        var defaultVideoDevice : AVCaptureDevice?
+        if let frontCameraDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: AVMediaType.video, position: .front) {
+            defaultVideoDevice = frontCameraDevice
+        }else{
+            print("What the fuck")
+        }
+        var videoDeviceInput : AVCaptureDeviceInput?
+        do{
+            videoDeviceInput = try AVCaptureDeviceInput(device: defaultVideoDevice!)
+            if session!.canAddInput(videoDeviceInput!){
+                session!.addInput(videoDeviceInput!)
+                videoPreviewLayer = AVCaptureVideoPreviewLayer(session: session)
+                videoPreviewLayer!.videoGravity = AVLayerVideoGravity.resizeAspect
+                videoPreviewLayer!.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+                self.view.layer.addSublayer(videoPreviewLayer!)
+                session!.startRunning()
+            }
+        } catch let error1{
+            print("Even more what the fuck \(error1)")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        videoPreviewLayer!.frame = self.view.bounds
     }
-
 
 }
 
