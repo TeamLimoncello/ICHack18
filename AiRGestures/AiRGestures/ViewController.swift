85f610e545858320316a1d353c8a46b5fd8e9b6b
Lewis Bell <lewisbell999@gmail.com>
2018-01-27 10:50:02 +0000

    Initialise Xcode project

---
+++ AiRGestures/AiRGestures/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AiRGestures
+//
+//  Created by Lewis Bell on 27/01/2018.
+//  Copyright © 2018 Lewis Bell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
