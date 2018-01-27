e6aaf8e9137f81911806628b18f75b5f6c3d2f50
Brendon Warwick <bluey31@dyn892-64.eduroam.ic.ac.uk>
2018-01-27 13:46:46 +0000

    init xcode project for server

---
+++ server/AirGestures/AirGestures/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AirGestures
+//
+//  Created by Brendon Warwick on 27/01/2018.
+//  Copyright © 2018 Team Limoncello. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ server/AirGestures/AirGestures/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AirGestures
+//
+//  Created by Brendon Warwick on 27/01/2018.
+//  Copyright © 2018 Team Limoncello. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
